f41349f55224f7c9537224ffebfdaa23331ad972
roman-russkikh <121030661+roman-russkikh@users.noreply.github.com>
2023-11-25 15:19:02 +0100

    Coins init amount added

--- Assets/CodeBase/Data/PlayerInventory.cs
+++ Assets/CodeBase/Data/PlayerInventory.cs
@@ -9,11 +9,14 @@ namespace CodeBase.Data
     public class PlayerInventory
     {
         [SerializeField] public List<int> _ownedCarIds;
+        [SerializeField] public int _coinsAmount;
 
         public PlayerInventory()
         {
             var carAmountInCollection = Game.CarsStorage.Cars.Count;
             _ownedCarIds = new List<int>(carAmountInCollection);
+
+            _coinsAmount = 50;
         }
     }
 }
