a95b50543e0888450c54e86ee9aeaad2eba3868c
roman-russkikh <121030661+roman-russkikh@users.noreply.github.com>
2023-11-25 11:23:37 +0100

    Core added

---
+++ Assets/CodeBase/UI/Services/Factory/UIFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using CodeBase.Services.PersistentProgress;
+using CodeBase.Services.StaticData;
+using CodeBase.StaticData.Windows;
+using CodeBase.UI.Services.Windows;
+using CodeBase.UI.Windows.Shop;
+using UnityEngine;
+
+namespace CodeBase.UI.Services.Factory
+{
+  public class UIFactory : IUIFactory
+  {
+    private const string UIRootPath = "UIRoot";
+ 
+    private readonly IStaticDataService _staticData;
+    
+    private Transform _uiRoot;
+    private readonly IPersistentProgressService _progressService;
+   
+
+    public UIFactory( IStaticDataService staticData, IPersistentProgressService progressService
+      )
+    {
+      _staticData = staticData;
+      _progressService = progressService;
+    }
+
+    public void CreateShop()
+    {
+      WindowConfig config = _staticData.ForWindow(WindowId.Shop);
+      ShopWindow window = Object.Instantiate(config.Template, _uiRoot) as ShopWindow;
+      window.Construct(_progressService);
+    }
+
+    public async Task CreateUIRoot()
+    {
+      /*GameObject root = await _assets.Instantiate(UIRootPath);
+      _uiRoot = root.transform;*/
+    }
+  }
+}
---
+++ Assets/CodeBase/UI/Windows/Shop/RewardedAdItem.cs
@@ -0,0 +1,42 @@
+﻿using CodeBase.Services.PersistentProgress;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Windows.Shop
+{
+  public class RewardedAdItem : MonoBehaviour
+  {
+    public Button ShowAdButton;
+    public GameObject[] AdActiveObjects;
+    public GameObject[] AdInactiveObjects;
+    
+    
+    private IPersistentProgressService _progressService;
+
+    public void Construct(IPersistentProgressService progresService)
+    {
+      _progressService = progresService;
+    }
+
+    public void Initialize()
+    {
+      ShowAdButton.onClick.AddListener(OnShowAdClicked);
+      RefreshAvailableAd();
+    }
+
+    public void Subscribe()
+    { }
+
+    public void Cleanup() 
+    { }
+
+    private void OnShowAdClicked() 
+    { }
+    
+
+    private void RefreshAvailableAd()
+    {
+      
+    }
+  }
+}
