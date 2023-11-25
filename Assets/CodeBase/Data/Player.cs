6ceaf01534900fac88ee06a37788e6721e31924d
roman-russkikh <121030661+roman-russkikh@users.noreply.github.com>
2023-11-25 14:53:29 +0100

    player data updated wip

--- Assets/CodeBase/Data/Player.cs
+++ Assets/CodeBase/Data/Player.cs
@@ -7,6 +7,12 @@ namespace CodeBase.Data
     public class Player
     {
         [SerializeField] private string _id = "";
-        
+        [SerializeField] private PlayerData _playerData;
+
+        public Player(string id)
+        {
+            _id = id;
+            _playerData = new PlayerData();
+        }
     }
 }
