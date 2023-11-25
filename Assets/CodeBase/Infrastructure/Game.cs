6e541e3e69add26b888793e52999049571594897
roman-russkikh <121030661+roman-russkikh@users.noreply.github.com>
2023-11-25 14:13:38 +0100

    car storage added to game code

--- Assets/CodeBase/Infrastructure/Game.cs
+++ Assets/CodeBase/Infrastructure/Game.cs
@@ -2,15 +2,18 @@ using CodeBase.Infrastructure.States;
 using CodeBase.Logic;
 using CodeBase.Services;
 using CodeBase.Services.Input;
+using UnityEngine;
 
 namespace CodeBase.Infrastructure
 {
   public class Game
   {
     public GameStateMachine StateMachine;
+    public static ScriptableObject CarsStorage;
 
-    public Game(ICoroutineRunner coroutineRunner, LoadingCurtain curtain)
+    public Game(ICoroutineRunner coroutineRunner, LoadingCurtain curtain, ScriptableObject carsStorage)
     {
+      CarsStorage = carsStorage;
       StateMachine = new GameStateMachine(new SceneLoader(coroutineRunner), curtain, AllServices.Container);
     }
   }
